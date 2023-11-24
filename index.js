dddb0902ec155b9ea4f9df37a256436b402a4484
mrjaat-tech <ankurtehlan2000@gmail.com>
2023-11-24 20:31:42 +0530

    Initial Commit

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import myJson from "./data.json" assert { type: "json" };
 (async () => {
   
   let data = [];
+  var myObj = {};  
     for (let i = 0; i < myJson.length; i++) {
       // const url = `https://automechanika.messefrankfurt.com/frankfurt/en/ausstellersuche.html?q=${urls[i]}`;
       let url = `https://boodmo.com/search/${myJson[i].name}`;
@@ -25,48 +26,41 @@ import myJson from "./data.json" assert { type: "json" };
         // Fetch the element with class "// a-link--no-focus"
         let quote = document.querySelector(".product-item-list__images").getAttribute("href");
         // fetching Second Data Code Start here
-
         return quote ;
       });
       // run the handleSearch Function
       // console.log(handleSearch);
-      let skuSearch = handleSearch;
-      let skuData = {
-        "sku":skuSearch,
-      }
-      data.push(skuData)
-      // console.log(data.sku);
+      // console.log(myJson[i].name);
+      let nextPageUrl = handleSearch;
+      myObj.sku = myJson[i].name
+      
+      
+
       
       // second Page Data fetching code start here
 
-      // const page2 = await browser.newPage();
-      // for (let i = 0; i < data.length; i++) {
+      const page2 = await browser.newPage();
         
-      //   await page2.goto(`https://boodmo.com${data[i].sku}`, { waitUntil: "domcontentloaded" });
+        await page2.goto(`https://boodmo.com${nextPageUrl}`, { waitUntil: "domcontentloaded" });
 
-      //   // wait for selector
-      //   await page2.waitForSelector(".part-info-price__mrp");
-      //   const handleSearchMrp = await page2.evaluate(() => {
-      //     // Fetch the element with class "// part-info-price__mrp
-      //     let Mrp = document.querySelector(".part-info-price__mrp").innerText
-      //     return Mrp ;
-      //   });
-      //   let mrpSearch = handleSearchMrp;
-      //   let mrpData = {
-      //     "mrp":mrpSearch,
-      //   }
-      //   data.push(mrpData)
-      // }
+        // wait for selector
+        await page2.waitForSelector(".part-info-price__mrp");
+        const handleSearchMrp = await page2.evaluate(() => {
+          // Fetch the element with class "// part-info-price__mrp
+          let Mrp = document.querySelector(".part-info-price__mrp").innerText
+          return Mrp ;
+        });
+        let mrpSearch = handleSearchMrp;
+        // console.log(mrpSearch);
+        myObj.price = mrpSearch;
+        data.push(myObj);
         
         await browser.close();
-        for (let i = 0; i < data.length; i++) {
-          console.log(data[i].sku);
-        }
     }
-
   }
-)();
 
+
+)();
 
 
 
