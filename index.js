ec698c839a152c357dbdb817c01c6eab2d758061
mrjaat-tech <ankurtehlan2000@gmail.com>
2023-11-24 19:26:42 +0530

    first commit

--- index.js
+++ index.js
@@ -1,113 +1,74 @@
 import puppeteer from "puppeteer";
 import myJson from "./data.json" assert { type: "json" };
-
 // Try to automate Start
 
 (async () => {
-  // const urls = ["3D INTERNATIONAL LLC", "247Lighting"];
-  for (let i = 0; i < myJson.length; i++) {
-    // const url = `https://automechanika.messefrankfurt.com/frankfurt/en/ausstellersuche.html?q=${urls[i]}`;
-    let url = `https://automechanika.messefrankfurt.com/frankfurt/en/ausstellersuche.html?q=${myJson[i].name}`;
-    const browser = await puppeteer.launch({
-      headless: false,
-      defaultViewport: null,
-    });
-    const page = await browser.newPage();
+  
+  let data = [];
+    for (let i = 0; i < myJson.length; i++) {
+      // const url = `https://automechanika.messefrankfurt.com/frankfurt/en/ausstellersuche.html?q=${urls[i]}`;
+      let url = `https://boodmo.com/search/${myJson[i].name}`;
+      const browser = await puppeteer.launch({
+        headless: false,
+        // headless: 'new',
+        defaultViewport: null,
+      });
+      const page = await browser.newPage();
 
-    await page.goto(`${url}`, { waitUntil: "domcontentloaded" });
+      await page.goto(`${url}`, { waitUntil: "domcontentloaded" });
 
-    // Wait until class [ .a-link--no-focus ] is load
-    await page.waitForSelector(".a-link--no-focus");
+      // Wait until class [ .a-link--no-focus ] is load
+      await page.waitForSelector(".product-item-list__images");
 
-    // handling page data and print using query selector and click on href link
-    const handleSearch = await page.evaluate(() => {
-      // Fetch the element with class "// a-link--no-focus"
-      let quote = document.querySelector(".a-link--no-focus").click();
-      return { quote };
-    });
+      // handling page data and print using query selector and click on href link
+      const handleSearch = await page.evaluate(() => {
+        // Fetch the element with class "// a-link--no-focus"
+        let quote = document.querySelector(".product-item-list__images").getAttribute("href");
+        // fetching Second Data Code Start here
 
-    // run the handleSearch Function
-    console.log(handleSearch);
+        return quote ;
+      });
+      // run the handleSearch Function
+      // console.log(handleSearch);
+      let skuSearch = handleSearch;
+      let skuData = {
+        "sku":skuSearch,
+      }
+      data.push(skuData)
+      // console.log(data.sku);
+      
+      // second Page Data fetching code start here
 
-    // After redirect to second Page query selector code start here
+      // const page2 = await browser.newPage();
+      // for (let i = 0; i < data.length; i++) {
+        
+      //   await page2.goto(`https://boodmo.com${data[i].sku}`, { waitUntil: "domcontentloaded" });
 
-    // Wait until class [ .ex-contact-box__contact-btn ] is load
-    await page.waitForSelector(".ex-contact-box__contact-btn");
+      //   // wait for selector
+      //   await page2.waitForSelector(".part-info-price__mrp");
+      //   const handleSearchMrp = await page2.evaluate(() => {
+      //     // Fetch the element with class "// part-info-price__mrp
+      //     let Mrp = document.querySelector(".part-info-price__mrp").innerText
+      //     return Mrp ;
+      //   });
+      //   let mrpSearch = handleSearchMrp;
+      //   let mrpData = {
+      //     "mrp":mrpSearch,
+      //   }
+      //   data.push(mrpData)
+      // }
+        
+        await browser.close();
+        for (let i = 0; i < data.length; i++) {
+          console.log(data[i].sku);
+        }
+    }
 
-    // Print page data using query selector
-
-    const getCustomerData = await page.evaluate(() => {
-      // Fetch the element with class and print "// .ex-contact-box__contact-btn"
-      let mail = document
-        .querySelector(".ex-contact-box__contact-btn")
-        .getAttribute("href");
-      return { mail };
-    });
-
-    console.log(getCustomerData);
-    // After redirect to second Page query selector code end here
-
-    await browser.close();
   }
-})();
+)();
+
+
+
 
 // Try to automate End
 
-// const getQuotes = async () => {
-//   // Start a Puppeteer session with:
-//   // - a visible browser (`headless: false` - easier to debug because you'll see the browser in action)
-//   // - no default viewport (`defaultViewport: null` - website page will in full width and height)
-//   const browser = await puppeteer.launch({
-//     headless: false,
-//     defaultViewport: null,
-//   });
-
-//   // Open a new page
-//   const page = await browser.newPage();
-
-//   // On this new page:
-//   // - wait until the dom content is loaded (HTML is ready)
-//   let url =
-//     "https://automechanika-dubai.ae.messefrankfurt.com/dubai/en/exhibitor-search.html?q=Meko%20Auto%20Components%20Inc.";
-
-//   await page.goto(url, {
-//     waitUntil: "domcontentloaded",
-//   });
-
-//   // Wait until class [ .a-link--no-focus ] is load
-//   await page.waitForSelector(".a-link--no-focus");
-
-//   // handling page data and print using query selector and click on href link
-//   const handleSearch = await page.evaluate(() => {
-//     // Fetch the element with class "// a-link--no-focus"
-//     let quote = document.querySelector(".a-link--no-focus").click();
-//     return { quote };
-//   });
-
-//   // run the handleSearch Function
-//   console.log(handleSearch);
-
-//   // After redirect to second Page query selector code start here
-
-//   // Wait until class [ .ex-contact-box__contact-btn ] is load
-//   await page.waitForSelector(".ex-contact-box__contact-btn");
-
-//   // Print page data using query selector
-
-//   const getCustomerData = await page.evaluate(() => {
-//     // Fetch the element with class and print "// .ex-contact-box__contact-btn"
-//     let mail = document
-//       .querySelector(".ex-contact-box__contact-btn")
-//       .getAttribute("href");
-//     return { mail };
-//   });
-
-//   console.log(getCustomerData);
-//   // After redirect to second Page query selector code end here
-
-//   // Close the browser
-//   await browser.close();
-// };
-
-// // Start the scraping
-// getQuotes();
